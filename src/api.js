8ca48fb6960eb629d7a0d3e760787d84898cd049
Yasha Kozhaev <yasha.kozhaev1@mail.ru>
2025-09-24 15:56:47 +0300

    Внес правки

--- src/api.js
+++ src/api.js
@@ -4,12 +4,12 @@ export function getWeather(cityName) {
     const url = `${API.url}forecast?q=${cityName}&appid=${API.key}`;
     return fetch(url) 
         .then(response => {
+            console.log(response.ok);
             if(!response.ok) {
                 return Promise.reject(`Ошибка API ${response.status}`);
             }
-            const data = response.json();
-            console.log(data);
-            return data;
+
+            return response.json();
         }) 
 }
 
